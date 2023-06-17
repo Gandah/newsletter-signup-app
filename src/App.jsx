d07ec19356719f03d6392e5a473b0971bd61d0ea
ganderson <gandahkelvin33@gmail.com>
2023-06-17 22:06:38 +0000

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; 
 import Form from "./components/Form";
-import Success from "./components/success";
+import Success from "./components/Success";
 import "./App.css";
 
 
